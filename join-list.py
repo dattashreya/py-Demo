4ca989ede10e7016635dbe8188fe46e02e5a5153
dattashreya <159568371+dattashreya@users.noreply.github.com>
2024-07-10 22:06:22 +0530

    Add files via upload

---
+++ join-list.py
@@ -0,0 +1,11 @@
+# looping join
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+for x in list2:
+  list1.append(x)
+print(list1)
+
+# concatination list
+list3=['bb','cc','dd']
+list4=[2,3,4]
+print(list3+list4)
