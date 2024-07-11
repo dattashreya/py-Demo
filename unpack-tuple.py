5e366c52181dbc8b4c39e2bebe750009e6d06be3
dattashreya <159568371+dattashreya@users.noreply.github.com>
2024-07-12 00:57:47 +0530

    Add files via upload

---
+++ unpack-tuple.py
@@ -0,0 +1,9 @@
+arr = ("shreya","mona","rima")
+(a,b,c)=arr 
+print(a,b,c)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+print(green)
+print(yellow)
+print(red)
