e9dd79d24feb8a6193d6b0fdf4388480cd4b9300
dattashreya <159568371+dattashreya@users.noreply.github.com>
2024-07-12 00:15:32 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,24 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+print(type(thistuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
