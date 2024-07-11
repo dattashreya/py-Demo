d53a840698f8a51fb661382ff981490ceaba2171
dattashreya <159568371+dattashreya@users.noreply.github.com>
2024-07-12 01:04:32 +0530

    Add files via upload

---
+++ loop-tuple.py
@@ -0,0 +1,15 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+  
+# for loop
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+# while loop
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
