873b05dda2a944eccebbebdfb7effdf89b170462
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-05-12 22:49:59 +0530

    Inheritance In Constructor

---
+++ inheritanceInConstructor.py
@@ -0,0 +1,17 @@
+class C:
+    def __init__(self):
+        print("init method constructor")
+    def fun1(self):
+        print("func 1 works")
+    def fun2(self):
+        print("func 2 works")
+class D(C):
+    def fun3(self):
+        print("func 3 works")
+    def fun4(self):
+        print("func 4 works")
+d = D()
+d.fun3()
+d.fun4()
+d.fun1()
+d.fun2()
